0e152effc46c77b27126ebe9229a02b2b24ea855
jpainter <jpainter@cdc.gov>
2022-01-21 11:45:09 -0500

    stop data time libraries (pacman)

--- data_time_libraries.r
+++ data_time_libraries.r
@@ -1,13 +1,13 @@
 # libraries used with data_time projects
 library(pacman)
-p_load( officer , rvg ,tidyverse, scales, 
+p_load( officer , rvg ,tidyverse, scales,
         tidyfast, tidytable, progress ,
-        lubridate , 
-        readxl, patchwork, 
+        lubridate ,
+        readxl, patchwork,
         tsibble, fable, fabletools, feasts, prophet , fable.prophet ,
         slider, anomalize, brolgar ,
-        tsbox , CausalImpact , tibbletime , dygraphs , 
-        forecast, fpp3  , fabletools, 
+        tsbox , CausalImpact , tibbletime , dygraphs ,
+        forecast, fpp3  , fabletools,
         furrr, tictoc, magrittr, hrbrthemes, data.tree, igraph ,
         sf, mapview, GGally , ggExtra, plotly , sugrrants ,
         data.tree, igraph, progressr
