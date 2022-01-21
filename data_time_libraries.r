53fe113be3435d31c5257eddd53f4d42ccddd736
jpainter <jpainter@cdc.gov>
2022-01-21 11:37:26 -0500

    adding to repo

---
+++ data_time_libraries.r
@@ -0,0 +1,14 @@
+# libraries used with data_time projects
+library(pacman)
+p_load( officer , rvg ,tidyverse, scales, 
+        tidyfast, tidytable, progress ,
+        lubridate , 
+        readxl, patchwork, 
+        tsibble, fable, fabletools, feasts, prophet , fable.prophet ,
+        slider, anomalize, brolgar ,
+        tsbox , CausalImpact , tibbletime , dygraphs , 
+        forecast, fpp3  , fabletools, 
+        furrr, tictoc, magrittr, hrbrthemes, data.tree, igraph ,
+        sf, mapview, GGally , ggExtra, plotly , sugrrants ,
+        data.tree, igraph, progressr
+)
