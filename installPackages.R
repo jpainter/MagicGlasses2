470fa48f2f07534e4600dbd8ead23470f1aab950
jpainter <jpainter@cdc.gov>
2022-01-26 11:42:22 -0500

    install pacakages

--- installPackages.R
+++ installPackages.R
@@ -1,4 +1,9 @@
-libraries =  readLines( 'magicGlasses2Libraries.txt') 
+if (!require( 'pacman' )){
+  install.packages( 'pacman' , dep=TRUE )
+  if( !require( 'pacman' ) ) stop("Package not found")
+}
+
+libraries =  readLines( 'magicGlasses2Libraries.txt' ) 
 include = !( grepl( '#' , libraries ) | nchar( libraries ) == 0 ) 
 library.list = libraries[ include ]
-pacman::p_load( library.list , update = TRUE )
+pacman::p_load( char = library.list , update = TRUE )
