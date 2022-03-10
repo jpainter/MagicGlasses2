2aee46cde6b6e49347a302de57b19029cafc1867
jpainter <jpainter@cdc.gov>
2022-03-10 16:47:56 -0500

    renv

--- installPackages.R
+++ installPackages.R
@@ -9,11 +9,20 @@ if (!require( 'devtools' )){
 }
 
 # latest packages required for describer
-devtools::install_github("glin/reactable")
-devtools::install_github("kcuilla/reactablefmtr")
-devtools::install_github("agstn/describer")
+# devtools::install_github("glin/reactable")
+# devtools::install_github("kcuilla/reactablefmtr")
+# devtools::install_github("agstn/describer")
 
+# MagicGlasses2Libraries
 libraries =  readLines( 'magicGlasses2Libraries.txt' ) 
 include = !( grepl( '#' , libraries ) | nchar( libraries ) == 0 ) 
-library.list = libraries[ include ]
-pacman::p_load( char = library.list , update = TRUE )
+library.list = trimws( libraries[ include ] )
+
+# pacman::p_load( char = library.list , update = TRUE )
+
+renv::install( packages = library.list )
+
+# save libraries
+renv::snapshot()
+
+renv::restore(packages = "renv")
