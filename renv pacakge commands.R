2f744cbaa06d617b57839e8e4cfb55fe5aeb5c91
John Painter <jpainter@cdc.gov>
2024-02-01 07:52:34 -0500

    Useful commands when restoring renv library

---
+++ renv pacakge commands.R
@@ -0,0 +1,30 @@
+
+
+# update packages so that not trying to install old version
+renv::update()
+
+# Restore packages except those that tend to be problematic
+renv::install(exclude = c('brolgar', 'bsts', 'rethinking', 'cmdstanr', 'terra'))
+
+# Check to see if any packages orphaned
+renv::status()
+
+# May need to install non-CRAN packages
+renv::install(  'class' )
+renv::install(  'terra' )
+renv::install( 'bsts' )
+
+getOption("repos")
+repos = c( CRAN = "https://cloud.r-project.org" , cmdstanr = "https://mc-stan.org/r-packages/" )
+options( repos = repos )
+install.packages( "cmdstanr" )
+cmdstanr::install_cmdstan( overwrite = TRUE )
+
+renv::install( c("coda","mvtnorm","devtools","loo", "dagitty") , prompt = FALSE )
+devtools::install_github("rmcelreath/rethinking")
+
+# recheck
+renv::status()
+
+# Save changes
+renv::snapshot( exclude = c( 'rethinking', 'cmdstanr' ) )
