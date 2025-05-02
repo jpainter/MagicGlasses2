93c014d8fe71041bd2fc56844d6ebff459aa52fb
Painter <bzp3@cdc.gov>
2025-05-02 15:00:40 -0400

    revised function matching dataElements and category combos from separate lists

--- prepareDataset.R
+++ prepareDataset.R
@@ -1,4 +1,6 @@
 
+# Match dataElements and category combos from data_element formula, which may not have 
+# updated category combo if a dataset was revised with new categories  
 translate_dataset = function( data , formula_elements ){
   cat('\n* translate_dataset:')
   
@@ -44,6 +46,58 @@ translate_dataset = function( data , formula_elements ){
   #                  )
   
   # d.ts = df_pre_ts( dd )
+  
+  return( dd )
+}
+
+# revised function matching dataElements and category combos from separate lists
+translate_dataset_2 = function( data , dataElements, categories  ){
+  
+  cat('\n* translate_dataset_2:')
+  
+
+  #Testing 
+  # saveRDS( categories, 'categories.rds')
+  
+  # Expand to one row per linked pair
+  cat( "\n- expanding categories table")
+  
+  categories_expanded <- categories %>%
+    mutate(
+      cat_list = str_split( Categories, " ;\\n "),
+      opt_list = str_split( categoryOptionCombo.ids, " ;\\n ")
+    ) %>%
+    mutate(
+      paired = map2( cat_list, opt_list, ~ tibble(
+        Category = trimws(.x),
+        categoryOptionCombo.id = trimws(.y)
+      ))
+    ) %>%
+    select(categoryCombo.id, categoryCombo, paired) %>%
+    unnest(paired)
+  
+
+  # Data may contain mix of elements with and without categories. 
+  cat( "\n- matching dataElement with dataElelemnt.id and 
+       categoryOptionCombo with categoryOptionCombo.id")
+
+    dataElement_match = match(  data$dataElement  , 
+                                dataElements$dataElement.id )
+   
+    categories_match = match(  data$categoryOptionCombo  , 
+                               categories_expanded$categoryOptionCombo.id )
+  
+  
+    dd = data %>% 
+      rename( 
+        dataElement.id = dataElement , 
+        categoryOptionCombo.ids = categoryOptionCombo 
+        ) %>%
+      bind_cols( 
+        dataElements[ dataElement_match , "dataElement" ] , 
+        categories_expanded[ categories_match , "Category" ] 
+        )
+     
   
   return( dd )
 }
@@ -128,6 +182,7 @@ ous_tree = function( ous , ouLevels ){
 }
 
 data_leaves = function( d ){
+  
   cat('\n* determining effective leaf')
   data.leaves = d  %>%
     as_tibble() %>%
@@ -196,18 +251,27 @@ is_null_or_empty <- function(x) {
   is.null(x) || length(x) == 0 || (is.character(x) && x == "")
 }
 
-data_1 = function( data , formula_elements , ousTree , timing = FALSE  ){
+data_1 = function( data , 
+                   dataSets = NULL ,
+                   formula_elements = NULL , 
+                   dataElements = NULL, 
+                   categories = NULL, 
+                   ousTree = NULL , 
+                   timing = FALSE  ){
   cat('\n* prepareDataset.R preparing data_1:')
   
   #   # TESTING
   # saveRDS( data , 'data.rds' )
+  # saveRDS( dataSets , 'dataSets.rds' )
   # saveRDS( formula_elements , 'formula_elements.rds' )
+  # saveRDS( dataElements , 'dataElements.rds' )
+  # saveRDS( categories , 'categories.rds' )
   # saveRDS( ousTree , 'ousTree.rds' )
   
   if ( ! 'COUNT' %in% names( data )) return()
   
   # if periodType missing, assume Monthly
-  if ( ! "periodType" %in% names(x) ) formula_elements$periodType = "Monthly"
+  if ( ! "periodType" %in% names( data ) ) formula_elements$periodType = "Monthly"
   ptype = min( formula_elements$periodType , na.rm = T)
   if ( is_null_or_empty( ptype ) ) ptype = "Monthly" # if missing, assume Monthly 
   cat( '\n - ptype is' , ptype )
@@ -218,39 +282,33 @@ data_1 = function( data , formula_elements , ousTree , timing = FALSE  ){
     if ( grepl( "monthly" , ptype , ignore.case = T)  ) p = "Month"
   }
   
-  cat( '\n - periodType is' , p )
+  # cat( '\n - periodType is' , p )
   
   cat('\n - translate_dataset:')
   
-  if (! 'categoryOptionCombo' %in% names( data )) data = data %>% mutate( categoryOptionCombo = NA )
+  if (! 'categoryOptionCombo' %in% names( data )){
+    
+    cat( "\n - 'categoryOptionCombo' %in% names( data ): FALSE ")
+    data = data %>% mutate( categoryOptionCombo = NA )
+    
+  } 
   
-  # if ( timing ) tic()
-  # dd = translate_dataset( data , formula_elements )
-  # if ( timing ) toc() 
-  # 
-  # cat('\n - df_pre_ts:')
-  # if ( timing ) tic()
-  # d = df_pre_ts( dd , period = p  )
-  # if ( timing ) toc()
-  # 
-  # cat('\n - df_ts:')
-  # if ( timing ) tic()
-  # d.ts = df_ts( d , period = p ) 
-  # if ( timing ) toc()
-  # 
-  # cat('\n - data_leaves')
-  # 
-  # cat('\n - combining data.leaves, outTree, and ouLevels')
-  # if ( timing ) tic()
-  # d. = d.ts %>%
-  # left_join( data.leaves , by = c( 'orgUnit' , 'dataElement.id' ) )  %>%
-  # left_join( ousTree , by = 'orgUnit')  %>%
-  # mutate( original = SUM , value = !is.na( SUM ))
-  
-  
-  # %>%
-  # left_join( ouLevels %>% select( level, levelName) , by = 'level' )
-# glimpse( d. )
+  # prepare list of datasets associated with each dataElement
+  dataSetElements = dataSets %>% 
+      unnest( dataSetElements.id, names_sep = "_" ) %>% 
+      select( c(1:3,5) ) %>% 
+      rename( dataElement.id = dataSetElements.id_dataElement ) %>%
+      # ensure that dataSet and dataElements are atomic vectors
+      mutate(
+        dataElement.id = as.character( dataElement.id$id )
+      ) %>%
+      group_by( dataElement.id ) %>%
+      summarise( 
+            n_datasets = n() ,
+            dataSet.ids = paste( dataSet.id ,  collapse = " ;\n"),
+            dataSet = paste( dataSet ,  collapse = " ;\n")
+          )  %>% arrange( - n_datasets  )
+
   if ( timing ) tic()
     
   d. =  data  %>%
@@ -258,13 +316,15 @@ data_1 = function( data , formula_elements , ousTree , timing = FALSE  ){
     filter( !is.na( SUM ) ) %>%
     # select( dataElement.id , categoryOptionCombo.ids , orgUnit , period ,  COUNT , SUM  ) %>%
     # rename( dataElement = dataElement.id , categoryOptionCombo = categoryOptionCombo.ids ) %>%
-    translate_dataset( . , formula_elements ) %>%
+    # translate_dataset( . , formula_elements ) %>%
+    translate_dataset_2( . ,  dataElements , categories ) %>%
+    left_join( dataSetElements , by = 'dataElement.id') %>%
     left_join( ousTree , by = 'orgUnit') 
     
-    data.leaves = data_leaves( d. )
+    data.leaves = data_leaves( d. ) 
     
 
-  if ( timing ) cat( "/n - d." , toc()$callback_msg )
+  if ( timing ) cat( "\n - d." , toc()$callback_msg )
     
   if ( timing ) tic()
   d.. = 
@@ -272,6 +332,7 @@ data_1 = function( data , formula_elements , ousTree , timing = FALSE  ){
     table.express::left_join( setDT( data.leaves ) , orgUnit , dataElement.id  )  %>%
     # table.express::left_join( setDT( ousTree ) , orgUnit )  %>%
     as_tibble() %>%
+    select( - i.leaf ) %>%
     df_pre_ts( . , period = p  ) %>%
     df_ts( . , period = p ) %>%
     mutate( original = SUM , value = !is.na( SUM )) 
